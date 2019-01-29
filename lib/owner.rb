e58b8f94c791bab3685aabe6c73fef948bf2a49f
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-29 02:13:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,37 @@
 class Owner
-  attr_reader :species
-  def initialize(species)
-    @species = species
+  attr_accessor   :name
+  attr_reader :species , :pets,  :name
+  @@all= []
+    def initialize(species)
+      @species = species
+      @pets= {:fishes=>[] ,:cats=>[], :dogs=>[]}
+      @@all << species
+    end
+#class methods only
+  def self.count
+    @@all.length
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  #Instacne methods only
+  def say_species
+    return "I am a human."
+  end
+
+  def buy_fish(fish)
+
+@pets[:fishes] << fish
+
+  end
+
+
+
+
 end
