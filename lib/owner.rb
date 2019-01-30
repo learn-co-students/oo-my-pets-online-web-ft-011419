1c86a32350391d42a2ce1fdc4b3ed6624992e421
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-30 23:56:18 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,20 +1,31 @@
+require 'pry'
+
 class Owner
   
-  attr_accessor :name, :pets
+  attr_accessor :name, :pets, :species
   
   @@all = []
   
-  def initialize(pets, species)
+  def initialize(pets, species, name)
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
+    @@count = self.count
   end  
 
   def self.all 
     @@all 
   end  
  
+  def name 
+    owner.name = name 
+  end  
+ 
   def buy_fish
     @pets << fish
   end   
+  
+  def walk_dogs 
+  
+  end  
   
 end
