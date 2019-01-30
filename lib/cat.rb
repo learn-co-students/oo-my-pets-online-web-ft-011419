89e9c927f8823118095600ac451c77bfac28bda7
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 17:39:02 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
-  # code goes here
+  
+  @@all = []
+  
+attr_reader :name 
+attr_accessor :mood, :cat
+
+@cat = []
+
+def initialize(name)
+  @name = name
+  @cat = cat
+  @mood = "nervous"
+end
+
+def self
+  @@all
+end
+
+def self.save
+  @@all << self
+end
 end
