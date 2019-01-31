e52a0bae6f95f6740c67c5c830e414e38933c557
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 01:07:30 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,10 +64,14 @@ class Owner
   end  
   
   def sell_pets 
-    pets.each do |pet|
-      
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end  
   end 
-  
+
   def list_pets 
     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end  
