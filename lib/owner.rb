84e47bfaa604ac2414c2314fbc7896b0afe763f3
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 01:00:33 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,8 +68,7 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dogs, and #{pets[:cats].count}."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end  
-    
-  
+ 
 end
